684da82a18d95e2db37726dd52f8a5bcb72bb83b
weiting_ding <infl55@hotmail.com>
2020-06-01 20:42:00 +0800

    tmp

--- js/script.js
+++ js/script.js
@@ -11,17 +11,15 @@ async function getJSON(url){
 }
 
 // console.log(getJSON(userURL));
+const bag=[];
 async function getRandomPeople(url){
-    const bag=[];
+    
     for(let i=0; i<12; i++){
         const item = await getJSON(url);
         bag.push(item.results[0]);
     }
-
-  
     return Promise.all(bag);
-  
-  }
+}
 
 
 function generateHTML(data) {
@@ -34,11 +32,12 @@ function generateHTML(data) {
             <img class="card-img" src=${person.picture.large} alt="profile picture">
         </div>
         <div class="card-info-container">
-            <h3 id="name" class="card-name cap">${person.name.first} ${person.name.last}</h3>
+            <h3 id="${person.name.first}${person.name.last}" class="card-name cap">${person.name.first} ${person.name.last}</h3>
             <p class="card-text">${person.email}</p>
             <p class="card-text cap">${person.location.city}, ${person.location.state}</p>
         </div>        
       `;
+      div.createComment
     });
   }
 
@@ -46,3 +45,12 @@ function generateHTML(data) {
 // console.log(getRandomPeople(userURL));
 getRandomPeople(userURL)
   .then(generateHTML);
+
+
+
+gallery.addEventListener('click', (event) =>{
+    if (event.target.className != "gallery"){
+        // const person = bag.find(p=>p.name===)
+        console.log(event.target.querySelector);
+    }
+});
